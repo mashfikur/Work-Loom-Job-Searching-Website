64f8c65b6764b423755bb5e5029f5cb47be0dcfe
mashfikur <120697837+mashfikur@users.noreply.github.com>
2023-11-04 20:40:33 +0600

    router setup completed

---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,9 @@
+const ErrorPage = () => {
+  return (
+    <div>
+      <h3>Oops this is a error</h3>
+    </div>
+  );
+};
+
+export default ErrorPage;
---
+++ src/routes/router.jsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "../layout/Layout";
+import ErrorPage from "../pages/ErrorPage";
+import Home from "../pages/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout></Layout>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
+  },
+]);
+
+export default router;
